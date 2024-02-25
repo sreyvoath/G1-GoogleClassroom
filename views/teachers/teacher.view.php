1f2f3c7f667d714fabe614b628d2686679f642cb
CHA EM <cha.em@student.passerellesnumeriques.org>
2024-02-25 14:45:41 +0700

    pull code

--- views/teachers/teacher.view.php
+++ views/teachers/teacher.view.php
@@ -43,13 +43,8 @@
 			<script src="../../js/main.js"></script>
 			<div class="col-sm-6 col-xl-4">
 				<form class="bg-body shadow rounded p-2">
-<<<<<<< HEAD
-					<div class="input-group input-borderless" id="card">
-						<input class="form-control me-1 search" type="text" placeholder="Search instructor" id="search" value="">
-=======
 					<div class="input-group input-borderless">
 						<input class="form-control me-1" id="search" type="search" placeholder="Search instructor">
->>>>>>> 6f269b3f7c98dc718441686a2ca924f735de9651
 						<button type="button" class="btn btn-primary mb-0 rounded"><i class="fas fa-search"></i></button>
 					</div>
 				</form>
@@ -139,24 +134,6 @@
 							<h3 class="mb-0">My Classroom List</h3>
 						</div>
 						<?php foreach ($classes as $class) : ?>
-<<<<<<< HEAD
-							<tbody>
-								<tr>
-									<!-- Course item -->
-									<td>
-										<div class="d-flex align-items-center">
-											<!-- Image -->
-											<div class="w-100px">
-												<img src="../../assets/images/classes/<?= $class['image'] ?>" class="rounded" alt="">
-											</div>
-											<div class="mb-0 ms-2" >
-												<!-- Title -->
-												<h6><?= $class['title'] ?></h6>
-												<!-- Info -->
-												<div class="d-sm-flex">
-													<p class="h6 fw-light mb-0 small me-3"><i class="fas fa-table text-orange me-2"></i>0 lectures</p>
-													<p class="h6 fw-light mb-0 small"><i class="fas fa-check-circle text-success me-2"></i>0 Completed</p>
-=======
 							<?php
 							if ($class['archive'] == 0) :
 							?>
@@ -177,7 +154,6 @@
 														<p class="h6 fw-light mb-0 small me-3"><i class="fas fa-table text-orange me-2"></i>0 lectures</p>
 														<p class="h6 fw-light mb-0 small"><i class="fas fa-check-circle text-success me-2"></i>0 Completed</p>
 													</div>
->>>>>>> 6f269b3f7c98dc718441686a2ca924f735de9651
 												</div>
 											</div>
 										</td>
