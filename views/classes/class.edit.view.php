91369d9afff23157c63c92e402cb498d93c7bdde
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2024-02-26 07:47:48 +0700

    [imp] add class active on page

--- views/classes/class.edit.view.php
+++ views/classes/class.edit.view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>E-Classroom</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" />
 </head>
@@ -13,9 +13,9 @@
     <div class="container mt-5">
         <div class="row">
             <div class="col-2"></div>
-            <div class="col-8 d-flex gap-5 border border-info border-3 p-5" style="border-radius: 30px;">
+            <div class="col-8 d-flex gap-5 border border-info border-3 p-5 shadow-lg p-3 mb-5 bg-body rounded" style="border-radius: 40px;">
                 <div class="image p-1">
-                    <h3 class="bg-info p-2 text-white text-center" style="border-radius: 30px; margin-top:-53px; margin-left:-53px;">Update Class</h3>
+                    <h3 class="bg-info p-2 text-white text-center" style="border-radius: 10px; margin-top:-53px; margin-left:-53px;">Update Class</h3>
                     <img class="border border-info border-3 mt-3 " src="../../assets/images/classes/<?= $class['image'] ?>" alt="" style="width:270px; height:300px; border-radius: 20px; margin-left:-25px;">
                 </div>
                 <form method="POST" action="../../controllers/classes/class.update.controller.php" enctype="multipart/form-data" class="" style="width:100%; height:auto">
