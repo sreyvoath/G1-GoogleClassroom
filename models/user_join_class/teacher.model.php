8d68251e3b3fd115b162a4c027a3c1d7533f9b95
Koemsran <koemsran.phon@student.passerellesnumeriques.org>
2024-03-12 23:51:58 +0700

    [feat]  show info only for teacher join GGC-13

--- models/user_join_class/teacher.model.php
+++ models/user_join_class/teacher.model.php
@@ -13,3 +13,20 @@ function getTeachers()
     $statement->execute([":role"=>"teacher"]);
     return $statement->fetchAll();
 }
+// ========Get all teacher join class============
+function getTeachersJoin(int $class_id )
+{
+
+    global $connection;
+    $statement = $connection->prepare("select u.id, u.name, u.email, u.image, c.id, uj.user_id from users u 
+    inner join users_join_class uj on uj.user_id= u.id
+    inner join classes c on uj.class_id = c.id
+    where u.role = :role and uj.class_id = :class_id
+    ");
+    $statement->execute([
+        ":role"=>"teacher",
+        ":class_id"=> $class_id
+    ]);
+    return $statement->fetchAll();
+}
+
