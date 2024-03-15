caa734b570fb93739d3068222a1cf0aee9e6e8eb
KOEMSRAN.PHON <KOEMSRAN.PHON@PNC.LAN>
2024-03-15 11:22:58 +0700

    [feat] student submit and unsubmit assignment GGC-16

---
+++ controllers/assignment/assignment_student/unsubmit_assignment.controller.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+require_once '../../../database/database.php';
+require_once('../../../models/assignments/assignment.model.php');
+
+if (isset($_SESSION['assignment_submitted'])) {
+    $assignments = $_SESSION['assignment_submitted'];
+    $ass_id = $_SESSION['assign_id'];
+    $currentDateTime = (new DateTime())->format('Y-m-d H:i:s'); 
+
+    foreach($assignments as $assignment){
+        $id = $assignment['id'];
+        $status = false;
+        updateAssignStatus($id, $status) ;
+    }
+    header("Location: /assignment_student?id=$ass_id");
+
+}
+
+?>
