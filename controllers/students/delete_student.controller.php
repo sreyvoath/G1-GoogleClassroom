b8f002b51f7bde81a5748a397be048ec9f15d140
KOEMSRAN.PHON <KOEMSRAN.PHON@pnc.lan>
2024-03-22 20:29:53 +0700

    [feat] put score for student GGC-26

--- controllers/students/delete_student.controller.php
+++ controllers/students/delete_student.controller.php
@@ -3,13 +3,10 @@ require '../../database/database.php';
 require '../../models/user_join_class/student.model.php';
 session_start();
 $class_id = $_SESSION['class_id'];
-
 // ========check class id =======
 $user_id = $_GET['id'] ? $_GET['id'] : null;
 
 if (isset($user_id) and isset($class_id)) {
-
     deleteStudent($user_id, $class_id);
     header("Location: /people?id=$class_id");
 }
-?>
